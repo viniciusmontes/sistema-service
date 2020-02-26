cca21457c50a677f528876d3ea6a4dc66724a837
Vinicius Montes <viniciusmontes@outlook.com.br>
2020-02-26 15:24:32 -0300

    Logradouro Modificado

--- src/service/LogradouroService.java
+++ src/service/LogradouroService.java
@@ -1,5 +1,15 @@
 package service;
 
-public interface LogradouroService {
+import java.util.Collection;
 
+import domain.exception.LogradouroException;
+import domain.model.Bairro;
+import domain.model.Logradouro;
+import domain.model.Municipio;
+import domain.model.UFVO;
+
+public interface LogradouroService extends Service<Logradouro, LogradouroException>{
+	
+	
+	Collection<Logradouro> listar(Bairro bairro, Municipio municipio, UFVO uf) throws LogradouroException;
 }
--- src/service/impl/DefaultLogradouroService.java
+++ src/service/impl/DefaultLogradouroService.java
@@ -1,5 +1,50 @@
 package service.impl;
 
-public class DefaultLogradouroService {
+import java.util.Collection;
+
+import domain.exception.LogradouroException;
+import domain.model.Bairro;
+import domain.model.Logradouro;
+import domain.model.Municipio;
+import domain.model.UFVO;
+import repository.LogradouroDAO;
+import repository.jdbc.JdbcLogradouroDAO;
+import service.LogradouroService;
+
+public class DefaultLogradouroService implements LogradouroService {
+	
+	private LogradouroDAO dao = new JdbcLogradouroDAO(); 
+	
+	@Override
+	public void validar(Logradouro model) throws LogradouroException {
+		// TODO Auto-generated method stub
+		
+		model.validarMunicipio();
+		model.validarBairro();
+		model.validarNome();
+	}
+
+	@Override
+	public void salvar(Logradouro model) throws LogradouroException {
+		// TODO Auto-generated method stub
+		if (model.isNullId()) {
+			dao.inserir(model);
+		} else {
+			dao.alterar(model);
+		}
+	}
+
+	@Override
+	public void apagar(Logradouro model) throws LogradouroException {
+		// TODO Auto-generated method stub
+		dao.apagar(model);
+	}
+
+
+	@Override
+	public Collection<Logradouro> listar(Bairro bairro, Municipio municipio, UFVO uf) throws LogradouroException {
+		// TODO Auto-generated method stub
+		return dao.selecionar(uf, municipio, bairro);
+	}
 
 }
